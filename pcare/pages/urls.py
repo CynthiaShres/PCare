aaea4799fe966a27ee9f1db41a0a25d6a405a656
CynthiaShres <cynthiashrestha5@gmail.com>
2020-02-10 20:45:23 +0545

    created static folder and modified

--- pcare/pages/urls.py
+++ pcare/pages/urls.py
@@ -3,4 +3,13 @@ from . import views
 
 urlpatterns=[
     path('',views.index, name='index'),
+    path('home',views.home, name='index'),
+    path('about',views.index, name='index'),
+    path('contact',views.contact, name='index'),
+
+    path('healthy_diet',views.health, name='index'),
+    path('awareness',views.awareness, name='index'),
+    path('exercises',views.exercises, name='index'),
+    path('medicine',views.medicine, name='index'),
+    path('mental',views.mental, name='index'),
 ]
--- pcare/pages/views.py
+++ pcare/pages/views.py
@@ -3,11 +3,21 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse('<h1> This is my home page </h1>')
 
+def home(request):
+    return render(request,'pages/home.html')
 def index(request):
     return render(request,'pages/about.html')
+def contact(request):
+    return render(request,'pages/contact.html')
 
-def index(request):
+def health(request):
     return render(request,'careyourself/healthy_diet.html')
+def awareness(request):
+    return render(request,'careyourself/awareness.html')
+def exercises(request):
+    return render(request,'careyourself/exercises.html')
+def medicine(request):
+    return render(request,'careyourself/medicine.html')
+def mental(request):
+    return render(request,'careyourself/mental.html')
